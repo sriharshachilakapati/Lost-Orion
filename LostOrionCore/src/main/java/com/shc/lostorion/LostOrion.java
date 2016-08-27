6b952f05a5d0d11f63155a442a78bee1af737415
Sri Harsha Chilakapati <sriharshachilakapati@gmail.com>
2016-08-27 19:05:53 +0530

    Enable context color blending

--- LostOrionCore/src/main/java/com/shc/lostorion/LostOrion.java
+++ LostOrionCore/src/main/java/com/shc/lostorion/LostOrion.java
@@ -2,7 +2,10 @@ package com.shc.lostorion;
 
 import com.shc.silenceengine.core.Game;
 import com.shc.silenceengine.core.SilenceEngine;
+import com.shc.silenceengine.graphics.opengl.GLContext;
 import com.shc.silenceengine.input.Keyboard;
+
+import static com.shc.silenceengine.graphics.IGraphicsDevice.Constants.*;
 
 public class LostOrion extends Game
 {
@@ -12,6 +15,10 @@ public class LostOrion extends Game
         SilenceEngine.display.setTitle("LostOrion: SilenceEngine v1.0.1");
         SilenceEngine.display.setSize(1280, 720);
         SilenceEngine.display.centerOnScreen();
+
+        // Enable GL Context blending of colors
+        GLContext.enable(GL_BLEND);
+        GLContext.blendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
     }
 
     @Override

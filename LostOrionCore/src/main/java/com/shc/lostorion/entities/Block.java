14eb0ebb297c6eeabb6be8bea66bcc851e4ce2ab
Sri Harsha Chilakapati <sriharshachilakapati@gmail.com>
2016-08-28 23:29:48 +0530

    Implemented Collision Detection between Ship and Block

--- LostOrionCore/src/main/java/com/shc/lostorion/entities/Block.java
+++ LostOrionCore/src/main/java/com/shc/lostorion/entities/Block.java
@@ -3,6 +3,8 @@ package com.shc.lostorion.entities;
 import com.shc.lostorion.BatchComponent;
 import com.shc.lostorion.Resources;
 import com.shc.silenceengine.graphics.Sprite;
+import com.shc.silenceengine.math.geom2d.Rectangle;
+import com.shc.silenceengine.scene.components.CollisionComponent2D;
 import com.shc.silenceengine.scene.entity.Entity2D;
 
 /**
@@ -14,5 +16,10 @@ public class Block extends Entity2D
     {
         position.set(x, y);
         addComponent(new BatchComponent(new Sprite(Resources.Textures.TILE_BLOCKED), 1));
+
+        final float width = Resources.Textures.TILE_BLOCKED.getWidth();
+        final float height = Resources.Textures.TILE_BLOCKED.getHeight();
+
+        addComponent(new CollisionComponent2D(Resources.CollisionTags.BLOCK, new Rectangle(width, height)));
     }
 }
--- LostOrionCore/src/main/java/com/shc/lostorion/entities/Ship.java
+++ LostOrionCore/src/main/java/com/shc/lostorion/entities/Ship.java
@@ -5,6 +5,8 @@ import com.shc.lostorion.Resources;
 import com.shc.silenceengine.graphics.Sprite;
 import com.shc.silenceengine.input.Keyboard;
 import com.shc.silenceengine.math.Vector2;
+import com.shc.silenceengine.math.geom2d.Rectangle;
+import com.shc.silenceengine.scene.components.CollisionComponent2D;
 import com.shc.silenceengine.scene.entity.Entity2D;
 
 import static com.shc.silenceengine.input.Keyboard.*;
@@ -15,6 +17,7 @@ import static com.shc.silenceengine.input.Keyboard.*;
 public class Ship extends Entity2D
 {
     private Vector2 speed = new Vector2();
+    private CollisionComponent2D collision;
 
     public Ship(float x, float y)
     {
@@ -32,6 +35,40 @@ public class Ship extends Entity2D
 
         particles.addComponent(new BatchComponent(particleAnim, 0));
         addChild(particles);
+
+        final float width = Resources.Textures.SHIP.getWidth();
+        final float height = Resources.Textures.SHIP.getHeight();
+
+        addComponent(collision = new CollisionComponent2D(Resources.CollisionTags.SHIP, new Rectangle(width, height),
+                this::onCollision));
+    }
+
+    private void onCollision(Entity2D self, CollisionComponent2D other)
+    {
+        if (other.entity instanceof Block)
+        {
+            Rectangle tBounds = collision.polygon.getBounds();
+            Rectangle oBounds = other.polygon.getBounds();
+
+            final float iWidth = tBounds.getIntersectionWidth(oBounds);
+            final float iHeight = tBounds.getIntersectionHeight(oBounds);
+
+            if (iWidth >= iHeight)
+            {
+                if (position.y >= other.entity.position.y)
+                    position.y += iHeight;
+                else
+                    position.y -= iHeight;
+            }
+
+            if (iHeight >= iWidth)
+            {
+                if (position.x >= other.entity.position.x)
+                    position.x += iWidth;
+                else
+                    position.x -= iWidth;
+            }
+        }
     }
 
     @Override
